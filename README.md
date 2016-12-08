16dcb375b286cfac39836fec998d8f47fe097319
John Wiseheart <johnwiseheart@gmail.com>
2016-12-08 12:50:34 -0800

    Initial commit with working module

---
+++ README.md
@@ -0,0 +1,23 @@
+# twopicode-network-utils
+----
+
+##Instructions
+
+```
+import { requestFactory, GET, POST, POST_FILE, PUT, DELETE} from 'twopicode-network-utils'
+
+const get = requestFactory(GET)
+const post = requestFactory(POST)
+const post_file = requestFactory(POST_FILE)
+const put = requestFactory(PUT)
+const delete = requestFactory(DELETE)
+
+
+get('http://example.com/api').then(resp => {
+	console.log(resp)
+})
+```
+
+You can optionally pass a function to the factory that triggers a notification. The function takes a single string - the message to be printed.
+
+For any more information you'll have to read the source, sorry!
---
+++ src/index.js
@@ -0,0 +1,70 @@
+import 'whatwg-fetch'
+
+const GET = 'GET'
+const POST = 'POST'
+const POST_FILE = 'POSTFILE'
+const PUT = 'PUT'
+const DELETE = 'DELETE'
+
+const contentType = {
+  GET: 'application/x-www-form-urlencoded',
+  POST: 'application/json',
+  PUT: 'application/json',
+  DELETE: 'application/json',
+}
+
+const requestFactory = (meth, notify = null) => {
+  let _headers = {}
+
+  // We need this to be able to send formData
+  if (meth === POST_FILE) {
+    meth = POST
+  } else {
+    _headers['Content-Type'] = contentType[meth]
+  }
+
+  return (token, path, body = undefined, options = {}, headers = {}) => {
+    if (token) {
+      _headers['Authorization'] = 'Bearer ' + token
+    }
+
+    return fetch(path, {
+      ...options,
+      headers: {
+        ..._headers,
+        ...headers
+      },
+      method: meth,
+      body: body
+    }).then(resp => {
+      const contentType = resp.headers.get('content-type')
+      if (contentType !== 'application/json') {
+        const errorMessage = 'Received unexpected response from the server.'
+        const e = new Error(errorMessage)
+        e.errorMessage = errorMessage
+        throw e
+      }
+
+      if (resp.status >= 200 && resp.status < 300) {
+        return resp.json()
+      } else {
+        return resp.json().then((err) => {
+          let e = new Error(JSON.stringify(err))
+          e.status = resp.status
+          e.body = err
+          throw e
+        })
+      }
+    }).catch(e => {
+      if (!e.status) {
+        // Inject a status code for 'unknown'
+        e.status = -1
+        const msg = e.errorMessage || 'A network error occurred. Please try again.'
+        if (notify) notify(msg)
+      }
+      return Promise.reject(e)
+    })
+  }
+}
+
+module.exports = {requestFactory, GET, POST, POST_FILE, PUT, DELETE}
